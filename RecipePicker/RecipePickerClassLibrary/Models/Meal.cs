26cc3c46bb6ff533a403e600c6f46b13d456e9e7
Sam Clayton <sam.c98@hotmail.co.uk>
2023-05-06 17:24:04 +0100

    Cleaning up usings

--- RecipePicker/RecipePickerClassLibrary/Models/Meal.cs
+++ RecipePicker/RecipePickerClassLibrary/Models/Meal.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace RecipePickerClassLibrary.Models
+﻿namespace RecipePickerClassLibrary.Models
 {
     public enum Meal
     {
